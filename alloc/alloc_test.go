bd92755b30ebafb68e3603fc4159d750f3eb9ae6
Aleksandr Kramarenko <akramarenkov@yandex.ru>
2024-08-06 12:59:50 +0300

    reduce alloc make

--- alloc/alloc_test.go
+++ alloc/alloc_test.go
@@ -7,29 +7,9 @@ import (
 	"github.com/stretchr/testify/require"
 )
 
-func TestMake(t *testing.T) {
-	testMake(t, 30, func(number int) int { return number })
-	testMake(t, 30, func(number int) int { return number/2 + 1 })
-}
-
-func testMake(t *testing.T, power int, modifier func(number int) int) {
-	for number := 1; number <= 1<<power; number *= 2 {
-		length := modifier(number)
-
-		slice := make([]bool, length)
-
-		for id := range slice {
-			slice[id] = true
-		}
-
-		require.Len(t, slice, length)
-		require.Equal(t, length, cap(slice))
-	}
-}
-
 func TestMakeAppend(t *testing.T) {
 	testMakeAppend(t, 30, func(number int) int { return number })
-	testMakeAppend(t, 30, func(number int) int { return number/2 + 1 })
+	testMakeAppend(t, 30, func(number int) int { return number - 1 })
 }
 
 func testMakeAppend(t *testing.T, power int, modifier func(number int) int) {
@@ -47,7 +27,7 @@ func testMakeAppend(t *testing.T, power int, modifier func(number int) int) {
 
 		previous := slice
 
-		slice = append(slice, true)
+		slice = append(slice, true) //nolint:makezero
 		require.NotSame(t, unsafe.SliceData(previous), unsafe.SliceData(slice))
 	}
 }
