580188da4e722c37d2eecee41971cb4b6e36f57a
Aleksandr Kramarenko <akramarenkov@yandex.ru>
2024-08-06 12:46:58 +0300

    added makeappend test

--- alloc/alloc_test.go
+++ alloc/alloc_test.go
@@ -2,16 +2,17 @@ package alloc
 
 import (
 	"testing"
+	"unsafe"
 
 	"github.com/stretchr/testify/require"
 )
 
-func TestAllocCap(t *testing.T) {
-	testAllocCap(t, 32, func(number int) int { return number })
-	testAllocCap(t, 32, func(number int) int { return number/2 + 1 })
+func TestMake(t *testing.T) {
+	testMake(t, 30, func(number int) int { return number })
+	testMake(t, 30, func(number int) int { return number/2 + 1 })
 }
 
-func testAllocCap(t *testing.T, power int, modifier func(number int) int) {
+func testMake(t *testing.T, power int, modifier func(number int) int) {
 	for number := 1; number <= 1<<power; number *= 2 {
 		length := modifier(number)
 
@@ -23,5 +24,30 @@ func testAllocCap(t *testing.T, power int, modifier func(number int) int) {
 
 		require.Len(t, slice, length)
 		require.Equal(t, length, cap(slice))
+	}
+}
+
+func TestMakeAppend(t *testing.T) {
+	testMakeAppend(t, 30, func(number int) int { return number })
+	testMakeAppend(t, 30, func(number int) int { return number/2 + 1 })
+}
+
+func testMakeAppend(t *testing.T, power int, modifier func(number int) int) {
+	for number := 1; number <= 1<<power; number *= 2 {
+		length := modifier(number)
+
+		slice := make([]bool, length)
+
+		for id := range slice {
+			slice[id] = true
+		}
+
+		require.Len(t, slice, length)
+		require.Equal(t, length, cap(slice))
+
+		previous := slice
+
+		slice = append(slice, true)
+		require.NotSame(t, unsafe.SliceData(previous), unsafe.SliceData(slice))
 	}
 }
