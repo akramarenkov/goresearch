65c19031bd62a060ad7db94da6e358616cddb3ac
Aleksandr Kramarenko <akramarenkov@yandex.ru>
2024-08-07 23:03:40 +0300

    added append cap test

--- alloc/capacity_test.go
+++ alloc/capacity_test.go
@@ -7,15 +7,15 @@ import (
 	"github.com/stretchr/testify/require"
 )
 
-func TestCapacity(t *testing.T) {
-	testCapacity(t, 30, func(number int) int { return number })
-	testCapacity(t, 30, func(number int) int { return number - 1 })
-	testCapacity(t, 30, func(number int) int { return number - number%10 })
-	testCapacity(t, 30, func(number int) int { return number - number%100 })
-	testCapacity(t, 30, func(number int) int { return number - number%1000 })
+func TestMakeCapacity(t *testing.T) {
+	testMakeCapacity(t, 30, func(number int) int { return number })
+	testMakeCapacity(t, 30, func(number int) int { return number - 1 })
+	testMakeCapacity(t, 30, func(number int) int { return number - number%10 })
+	testMakeCapacity(t, 30, func(number int) int { return number - number%100 })
+	testMakeCapacity(t, 30, func(number int) int { return number - number%1000 })
 }
 
-func testCapacity(t *testing.T, power int, modifier func(number int) int) {
+func testMakeCapacity(t *testing.T, power int, modifier func(number int) int) {
 	for number := 1; number <= 1<<power; number *= 2 {
 		length := modifier(number)
 
@@ -29,4 +29,49 @@ func testCapacity(t *testing.T, power int, modifier func(number int) int) {
 		slice = append(slice, 1) //nolint:makezero
 		require.NotSame(t, unsafe.SliceData(previous), unsafe.SliceData(slice))
 	}
+}
+
+func TestAppendCapacity(t *testing.T) {
+	testAppendCapacity(t, 30, func(number int) int { return number })
+	testAppendCapacity(t, 30, func(number int) int { return number - 1 })
+	testAppendCapacity(t, 30, func(number int) int { return number - number%10 })
+	testAppendCapacity(t, 30, func(number int) int { return number - number%100 })
+	testAppendCapacity(t, 30, func(number int) int { return number - number%1000 })
+}
+
+func testAppendCapacity(t *testing.T, power int, modifier func(number int) int) {
+	equalities := 0
+	discrepancy := 0
+
+	same := 0
+	notSame := 0
+
+	for number := 1; number <= 1<<power; number *= 2 {
+		length := modifier(number)
+
+		slice := append([]byte(nil), make([]byte, length)...)
+
+		if cap(slice) == length {
+			equalities++
+		} else {
+			discrepancy++
+		}
+
+		require.Equal(t, length, len(slice))
+
+		previous := slice
+
+		slice = append(slice, 1) //nolint:makezero
+
+		if unsafe.SliceData(previous) == unsafe.SliceData(slice) {
+			same++
+		} else {
+			notSame++
+		}
+	}
+
+	require.NotZero(t, equalities)
+	require.NotZero(t, discrepancy)
+	require.NotZero(t, same)
+	require.NotZero(t, notSame)
 }
